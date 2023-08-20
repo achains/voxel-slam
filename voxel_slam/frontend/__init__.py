87f8f6fc6a4f9acd998737583e03d212deb076b9
achains <arthur.salio@gmail.com>
2023-08-20 16:04:11 +0300

    feat: VoxelFeature chain handler

--- voxel_slam/frontend/__init__.py
+++ voxel_slam/frontend/__init__.py
@@ -1,2 +1,5 @@
 import voxel_slam.frontend.voxel_feature_map
 from voxel_slam.frontend.voxel_feature_map import *
+
+import voxel_slam.frontend.voxel_feature_filters
+from voxel_slam.frontend.voxel_feature_filters import *
---
+++ voxel_slam/frontend/voxel_feature_filters.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+import numpy as np
+
+from abc import ABC, abstractmethod
+from sklearn.cluster import AgglomerativeClustering
+
+__all__ = ["NormalsFilter", "PlaneDistanceFilter", "EmptyVoxelsFilter"]
+
+
+class VoxelFilter(ABC):
+    @abstractmethod
+    def set_next(self, filter: VoxelFilter) -> VoxelFilter:
+        pass 
+
+    @abstractmethod
+    def filter(self, voxel_feature_map):
+        pass 
+
+
+class AbstractVoxelFilter(VoxelFilter):
+    _next_filter: VoxelFilter = None 
+
+    def set_next(self, filter: VoxelFilter) -> VoxelFilter:
+        self._next_filter = filter
+        return filter
+    
+    @abstractmethod
+    def filter(self, voxel_feature_map):
+        if self._next_filter:
+            self._next_filter.filter(voxel_feature_map)
+        
+        return None 
+    
+
+class NormalsFilter(AbstractVoxelFilter):
+    def __init__(self, cosine_distance_threshold) -> None:
+        self.cosine_distance_threshold = cosine_distance_threshold
+
+    def filter(self, voxel_feature_map):
+        self._filter_normals(voxel_feature_map)
+        return super().filter(voxel_feature_map)
+
+    def _filter_normals(self, voxel_feature_map):
+        for pose_to_points in voxel_feature_map.values():
+            normals = [plane.get_plane_equation()[:-1] for plane in pose_to_points.values()]
+            if len(normals) < 2:
+                continue
+            
+            clustering = AgglomerativeClustering(
+                n_clusters=None,
+                distance_threshold=self.cosine_distance_threshold,
+                metric="cosine",
+                linkage="single",
+                compute_distances=True 
+            ).fit(np.asarray(normals))
+
+            stable_plane_label = np.bincount(clustering.labels_).argmax()
+            outlier_plane_poses = np.asarray(list(pose_to_points.keys()))[clustering.labels_ != stable_plane_label]
+            for pose_id in outlier_plane_poses:
+                pose_to_points.pop(pose_id)
+
+
+class PlaneDistanceFilter(AbstractVoxelFilter):
+    def filter(self, voxel_feature_map):
+        self._filter_plane_distance(voxel_feature_map)
+        return super().filter(voxel_feature_map)
+    
+    def _filter_plane_distance(self, voxel_feature_map):
+        for pose_to_points in voxel_feature_map.values():
+            planes_d = [plane.get_plane_equation()[-1] for plane in pose_to_points.values()]
+            if len(planes_d) < 2:
+                continue
+
+            clustering_d = AgglomerativeClustering(
+                n_clusters=2,
+            ).fit(np.asarray(planes_d).reshape(-1, 1))
+
+            stable_plane_label = np.bincount(clustering_d.labels_).argmax()
+            outlier_plane_poses = np.asarray(list(pose_to_points.keys()))[clustering_d.labels_ != stable_plane_label]
+            for pose_id in outlier_plane_poses:
+                pose_to_points.pop(pose_id)
+
+
+class EmptyVoxelsFilter(AbstractVoxelFilter):
+    def __init__(self, min_voxel_poses=2) -> None:
+        self.min_voxel_poses = min_voxel_poses
+
+    def filter(self, voxel_feature_map):
+        self._filter_empty_voxels(voxel_feature_map)
+        return super().filter(voxel_feature_map)
+    
+    def _filter_empty_voxels(self, voxel_feature_map):
+        empty_voxels = []
+        for voxel_center, pose_to_points in  voxel_feature_map.items():
+            if len(pose_to_points) < self.min_voxel_poses:
+                empty_voxels.append(voxel_center)
+
+        for voxel_center in empty_voxels:
+            voxel_feature_map.pop(voxel_center) 
